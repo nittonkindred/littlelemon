fe673e648bee51471f12c71aa1def85dbb49b7eb
Kiều Trung Tín <mantinited@gmail.com>
2026-05-11 19:57:20 +0700

    fix name

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reservations/', include('reservations.urls')),
+    path('reservations/', include('restaurant.urls')),
 ]
